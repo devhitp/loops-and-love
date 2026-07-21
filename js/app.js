eb3d449d41ada8a38a81c628c5f46861e4b37410
Hitesh Pareek <hiteshpareek5107@gmail.com>
2026-07-22 00:38:43 +0530

    minor changes

--- js/app.js
+++ js/app.js
@@ -77,4 +77,19 @@ if (menuBtn) {
             icon.classList.add('fa-bars');
         }
     });
-}
+}
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 20){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
